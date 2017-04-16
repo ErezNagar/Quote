0271c9c47be2327f85b6298765dd88ef7f344cb7
Erez Nagar <erez.nagar@gmail.com>
2017-04-15 20:35:24 -0400

    fix1

--- app.js
+++ app.js
@@ -1,63 +1,48 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-const Firebase = require('firebase-client');
+var Firebase = require('firebase-client');
+var Quote = require("./quote");
+var commandParser = require("./commandParser");
+
+var quoteStore = new Firebase({
+    url: Quote.Definitions.FIREBASE_URL,
+    auth: Quote.Definitions.FIREBASE_TOKEN
+});
 // const Firebase = require('firebase');
 // var Firebase = require("Firebase");
 
-var Definitions = require("./definitions");
-var commandParser = require("./commandParser");
 commandParser = new commandParser();
 
-var app = express();
-var port = process.env.PORT || 8888;
+var QuoteApp = express();
 
-// app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// QuoteApp.use(bodyParser.json());
+QuoteApp.use(bodyParser.urlencoded({ extended: true }));
 
-app.post("/", function (req, res) {
-    console.log("Incoming slack quote v2...");
-    if (req.body.token !== Definitions.Constants.APP_TOKEN){
-        console.error("unothorized");
-        res.send("unothorized");
+QuoteApp.post("/", function (req, res) {
+    if (req.body.token !== Quote.Definitions.SLACK_TOKEN){
+        console.error(Quote.Constants.UNAUTHORIZED);
+        res.send(Quote.Constants.UNAUTHORIZED);
         return;
     }
 
-    //  route support, for example, adding user names, help
-
-    var quoteData = commandParser.parse(req.body.text);
-    if (!quoteData){
-        console.error(Definitions.Constants.COMMAND_ERROR);
-        res.send(Definitions.Constants.COMMAND_ERROR + " " + Definitions.Constants.HELP);
+    var quote = commandParser.parse(req.body.text);
+    if (!quote){
+        console.error(Quote.Constants.COMMAND_ERROR);
+        res.send(Quote.Constants.COMMAND_ERROR + " " + Quote.Constants.HELP);
         return;
     }
 
-    quoteData.timestamp = Date.now(); //getCurrentDate();
+    quote.timestamp = Date.now(); //getCurrentDate();
 
-    var firebase = new Firebase({
-      url: "https://funnycss.firebaseio.com",
-      auth: "LeTUNxwGc03CdCj61oh5cFKuRRnXu23ATbVuZjl8"
-    });
-
-    firebase.push("/quotes", quoteData).then(function(body){
-        console.log(body);
-        res.send("Quote saved.");
-    }).fail( function(err){
-        console.error("Data could not be saved." + err);
-        res.send("Error saving quote. Please try again.");
-    });
-
-    // var quotesRef = new Firebase("https://funnycss.firebaseio.com/quotes");
-    // quotesRef.push(quoteData, function(){
-    //     if (error) {
-            // console.error("Data could not be saved." + error);
-            // res.send("Error saving quote. Please try again.");
-            // return;
-    //     }
-    //
-        // res.send("Quote saved.");
-    // });
+    quoteStore.push("/quotes", quote)
+        .then(function(body){
+            res.send(Quote.Constants.SUCCESS);
+        }).fail( function(err){
+            console.error("Data could not be saved. " + err);
+            res.send(Quote.Constants.SAVE_ERROR);
+        });
 });
 
-app.listen(port, function () {
-  console.log("Example app listening on port " + port + "!");
+QuoteApp.listen(process.env.PORT || 8888, function () {
+  console.log("Quote listening on port " + (process.env.PORT || 8888) + "...");
 });
