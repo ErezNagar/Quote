ae94b8e0b95eb28c87fc8433c678a51bc5d421c3
Erez Nagar <erez.nagar@gmail.com>
2017-04-15 21:30:17 -0400

    adding new parser changes

---
+++ parser.js
@@ -0,0 +1,56 @@
+"use strict";
+
+var delimiters = {
+    QUOTE: '"',
+    AUTHOR: " By ",
+}
+
+class Parser {
+    constructor() {
+        this.getQuote = function(command){
+            if (command.replace(/[^/"]/g, "").length !== 2)
+                return null;
+
+            let quote = command.split(delimiters.QUOTE);
+            if (quote[0].length !== 3)
+                return null;
+
+            quote.shift();
+
+            if (quote[0].length === 0)
+                return null;
+
+            return quote[0];
+        };
+
+        this.getAuther = function(command){
+            command = command.replace(delimiters.AUTHOR, delimiters.AUTHOR.toLowerCase());
+
+            if (command.indexOf(delimiters.AUTHOR.toLowerCase()) === -1)
+                return null;
+
+            let author = command.replace(delimiters.AUTHOR.toLowerCase(), "");
+            if (author.length == 0)
+                return null;
+
+            return author.trim();
+        };
+    }
+
+    parseCommand(command) {
+        let quote, author;
+
+        if (!(quote = this.getQuote(command)))
+            return null;
+
+        if (!(author = this.getAuther(command.replace('"' + quote + '"', ""))))
+            return null;
+
+        return {
+            quote: quote,
+            author: author,
+        }
+    }
+}
+
+module.exports = Parser;
